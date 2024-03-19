99c4a41f5c6faaa25dadb3ec0d13cdeed3104887
Guadalupe Soria <A01710797@tec.mx>
2024-03-19 00:07:04 -0600

    Crear controlador de construcciones, uso de get_construir

--- minecraft/construcciones.routes.js
+++ minecraft/construcciones.routes.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const construccionesController = 
+    require('../controllers/construcciones.controller');
 const construcciones = [
     {
         nombre: "casa", 
         imagen: "https://i.blogs.es/7cfcd0/casas-en-minecraft/1366_2000.jpeg",
     }
 ];
-router.get('/construir', (request, response, next) => {
-  
+router.get('/construir', construccionesController.get_construir);
 
-  response.render('construir'); 
-
-});
 router.post('/construir', (request, response, next) => {
     console.log(request.body);
     construcciones.push(request.body);
