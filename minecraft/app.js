f8ac2c3c55610594189f27898c238ea8992740be
Guadalupe Soria <A01710797@tec.mx>
2024-03-18 20:21:31 -0600

    peticiones http y mandar respuesta

--- minecraft/app.js
+++ minecraft/app.js
@@ -7,24 +7,30 @@ const filesystem = require('fs');
 //en el archivo indicado en el primer parámetro
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
 
-setTimeout(() => {
+const te_hackie = () => {
     console.log('jojo te hackié');
-}, 7000);
+};
 
+//Código asíncrono: te_hackie se ejecuta en 7 segundos
+setTimeout(te_hackie, 7000);
 const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
 
+//Código asíncrono el arreglon se imprime en orden
 for (let item of arreglo) {
     setTimeout(() => {
         console.log(item);
     }, item);
 } 
 
+console.log("Esto se imprime antes de los números");
 
-// const http = require('http');
-// const server = http.createServer( (request, response) => {    
-//     console.log(request.url);
-//     response.setHeader('Content-Type', 'text/html');
-//     response.write("");
-//     response.end();
-// });
-// server.listen(3000);
+//http es un módulo de node con todas las funciones de un servidor web
+const http = require('http');
+
+const server = http.createServer( (request, response) => {    
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text/html');
+    response.write("hola mundo desde node");
+    response.end();
+});
+server.listen(3000);
