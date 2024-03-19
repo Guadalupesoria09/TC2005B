7d4d5ed9fe4a3aa651980af6d5814765fe2825ee
Guadalupe Soria <A01710797@tec.mx>
2024-03-18 18:57:05 -0600

    Evento onclick, cambio de imagen con el DOM, combio de boton con eventos del DOM, cambbio de imagenes de construccion de casa

--- minecraft/poe.js
+++ minecraft/poe.js
@@ -1,7 +1,26 @@
 const boton = document.getElementById("boton_construir");
-boton.onclick = () => { 
+const construir_casa = () => { 
     const imagen = document.getElementById("ImagenConstruir");
     imagen.src = "https://www.movilzona.es/app/uploads-movilzona.es/2022/02/casa-minecraft-inicio-primera-planta.jpg";
     
-    console.log(imagen);
+    boton.innerHTML = "Terminar casa";
+    boton.className = "button is-warning";
+    boton.onclick = terminar_casa;
 };
+const terminar_casa = () => { 
+    const imagen = document.getElementById("ImagenConstruir");
+    imagen.src = "https://i.ytimg.com/vi/VMV5h74Vy8Q/maxresdefault.jpg";
+    
+    boton.innerHTML = "Destruir casa";
+    boton.className = "button is-danger";
+    boton.onclick = destruir_casa;
+};
+const destruir_casa = () => { 
+    const imagen = document.getElementById("ImagenConstruir");
+    imagen.src = "https://static.planetminecraft.com/files/image/minecraft/texture-pack/2020/328/13784023_xl.jpg";
+    
+    boton.innerHTML = "Construir casa";
+    boton.className = "button is-primary";
+    boton.onclick = construir_casa;
+};
+boton.onclick = construir_casa;
