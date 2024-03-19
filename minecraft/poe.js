09d97dfb60402118e891a1a7c87d637a0857dff9
Guadalupe Soria <A01710797@tec.mx>
2024-03-18 18:40:16 -0600

    Cambiar imagen con el DOM

--- minecraft/poe.js
+++ minecraft/poe.js
@@ -1,4 +1,7 @@
 const boton = document.getElementById("boton_construir");
 boton.onclick = () => { 
-    console.log("click"); 
+    const imagen = document.getElementById("ImagenConstruir");
+    imagen.src = "https://www.movilzona.es/app/uploads-movilzona.es/2022/02/casa-minecraft-inicio-primera-planta.jpg";
+    
+    console.log(imagen);
 };
